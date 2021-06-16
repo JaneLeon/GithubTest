518674c653625f40cde2f34bc731d4e1562004fc
JaneLeon <85981766+JaneLeon@users.noreply.github.com>
2021-06-16 14:25:13 +0800

    Update main.kt

--- main.kt
+++ main.kt
@@ -1,7 +1,16 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
-    println("你好")
-    println("請問大名?")
-    val name = readLine()
-    println("您好 $name ! ")
-}d
+fun getStringLength(obj: Any): Int? {
+ if (obj is String) {
+ return obj.length
+ }
+ return null
+}
+fun main() {
+ fun printLength(obj: Any) {
+ println("'$obj' ......您的名字共有 ${getStringLength(obj) ?: "... 錯誤, 這不是
+一個字串"} 個字 ")
+ }
+ println("請問大名？")
+ val name = readLine()
+ println("您好， $name！")
+ printLength("$name")
+}
